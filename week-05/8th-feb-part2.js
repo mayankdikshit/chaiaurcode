e8709d43e2ea77a869aa9326a9805706e843776c
mayankdikshit <mayankdikshit711@gmail.com>
2025-02-16 20:52:07 +0530

    stack heap object confused

--- week-05/8th-feb-part2.js
+++ week-05/8th-feb-part2.js
@@ -1,25 +1,26 @@
-let fname = "mayank";
-
-let userName = fname;
-
-console.log(fname);
-console.log(userName);
-
-
-userName = "abhishek";
-
-console.log(fname);
-console.log(userName);
-
-
 p1 = {
     name : "mayank",
-    age : 26,
-    city : "meerut"
+    city : "meerut",
+    address : {
+        h : 2,
+        s : 1,
+    }
 }
 
 p2 = {
      ...p1
 }
 
+// p2 = {
+//     newName : p1.name,
+//     newCity : p1.city, 
+//     Newddress : p1.address,
+// }
+
+p2.Newname = "jerry";
+
+console.log(p1)
 console.log(p2)
+
+
+
